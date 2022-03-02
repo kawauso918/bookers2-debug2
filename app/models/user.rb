c34f6db8f07a4197761851a3c761dae7eb8aed18
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-03-02 02:29:07 +0000

    Add favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   attachment :profile_image, destroy: false
   has_many :books
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction,  length: {maximum: 50}
