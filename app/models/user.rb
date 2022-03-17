e342d94b64627683fb01f09c86fc8db50107ad12
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-03-17 01:17:34 +0000

    Add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction,  length: {maximum: 50}
 end
